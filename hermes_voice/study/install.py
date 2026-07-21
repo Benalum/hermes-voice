99edbb7995a428cdb6a09588f75feec8e87d793d
Alex Hartel <37965044+Benalum@users.noreply.github.com>
2026-07-21 14:09:42 -0600

    Install curriculum services with Study

--- hermes_voice/study/install.py
+++ hermes_voice/study/install.py
@@ -11,6 +11,9 @@ from fastapi.responses import FileResponse
 from hermes_voice.kit import session as sm
 from hermes_voice.kit.ports import ResponderPort
 from hermes_voice.study.api import create_study_router
+from hermes_voice.study.curriculum import foundation_curriculum_skeleton
+from hermes_voice.study.curriculum_api import create_curriculum_router
+from hermes_voice.study.curriculum_store import CurriculumStore
 from hermes_voice.study.pack_api import create_pack_router
 from hermes_voice.study.responder import StudyResponder
 from hermes_voice.study.store import StudyStore
@@ -19,8 +22,13 @@ MakeResponder = Callable[[Callable[[sm.Event], None]], ResponderPort]
 
 
 def install_study(app: FastAPI, store: StudyStore, web_dir: Path) -> None:
+    curriculum_store = CurriculumStore(store.paths)
+    curriculum_store.install_curriculum(foundation_curriculum_skeleton())
+
     app.include_router(create_study_router(store))
     app.include_router(create_pack_router(store))
+    app.include_router(create_curriculum_router(curriculum_store))
+    app.state.curriculum_store = curriculum_store
 
     @app.get("/study")
     async def study_page() -> FileResponse:
